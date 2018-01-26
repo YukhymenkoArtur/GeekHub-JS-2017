60e0407173f65fc99115ab99f779e06280358f37
YukhymenkoArtur <yukhymenko.artur@gmail.com>
2018-01-26 21:59:05 +0200

    .

--- HW_13/js/todo/index.js
+++ HW_13/js/todo/index.js
@@ -1,3 +1,9 @@
+//window.addEventListener('load', function () {
+//    taskController(document.getElementById('todo-list'));
+//});
+
+import taskController from './controller/task';
+import '../../css/task.css';
 window.addEventListener('load', function () {
     taskController(document.getElementById('todo-list'));
 });
